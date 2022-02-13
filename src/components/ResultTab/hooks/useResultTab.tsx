d65da944161f538029fe3ef2b80b5dec1db3d3fe
alvepoe <alve1507@yandex.ru>
2022-02-13 13:30:45 +0300

    minfix

--- src/components/ResultTab/hooks/useResultTab.tsx
+++ src/components/ResultTab/hooks/useResultTab.tsx
@@ -85,7 +85,9 @@ export const useResultTab = () => {
               })}
             </RadioGroup>
           ) : (
-            <Typography sx={{margin: '20px'}}>Radio group options weren't found</Typography>
+            <Typography sx={{ margin: '20px' }}>
+              Radio group options weren't found
+            </Typography>
           )
         }
       />
